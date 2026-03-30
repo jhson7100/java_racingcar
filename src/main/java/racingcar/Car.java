7a8d3344467bf347b06d7a49809a4e39f44d6f10
son jeong han <aassws3213@gmail.com>
2026-03-30 18:37:40 +0900

    자동차 클래스 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,21 @@
 package racingcar;
 
 public class Car {
+    String name;
+    int position = 0;
 
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int number) {
+        if (number >= 4) position++;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getPosition() {
+        return position;
+    }
 }
