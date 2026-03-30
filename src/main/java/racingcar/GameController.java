de771c69e24872b2471a9381465e85b6ec67346d
son jeong han <aassws3213@gmail.com>
2026-03-30 19:22:12 +0900

    게임 메인로직 구현

--- src/main/java/racingcar/GameController.java
+++ src/main/java/racingcar/GameController.java
@@ -1,5 +1,34 @@
 package racingcar;
 
-public class GameController {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
+public class GameController {
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private List<Car> cars = new ArrayList<>();
+
+    //게임 실행 메인 로직
+    public void run() {
+        String[] carNames = inputView.getCarNames();
+        for (String name : carNames) {
+            cars.add(new Car(name.trim()));
+        }
+
+        int attemptCount = inputView.getCount();
+
+        outputView.printExecutionResultMessage();
+        for (int i = 0; i < attemptCount; i++) {
+            playRound();
+            outputView.printRaceStatus(cars);
+        }
+    }
+
+    private void playRound() {
+        for (Car car : cars) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            car.move(randomNumber);
+        }
+    }
 }
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,5 +1,23 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
 
+    public void printExecutionResultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public void printRaceStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.print(car.getName() + " : ");
+
+            for (int i = 0; i < car.getPosition(); i++) {
+                System.out.print("-");
+            }
+
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
