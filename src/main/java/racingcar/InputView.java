4f0632ceee48a40c3df85ef4efcd67a5007c3a76
son jeong han <aassws3213@gmail.com>
2026-03-30 18:51:13 +0900

    입력 시 예외 케이스 처리기능 추가

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,6 +1,8 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     public String[] getCarNames() {
@@ -8,5 +10,30 @@ public class InputView {
         String input = Console.readLine();
         String[] names = input.split(",");
         return names;
+    }
+
+    private void validateCarNames(String[] names) {
+        List<String> nameList = new ArrayList<>();
+
+        for (String name : names) {
+            // 공백이 들어있는지 확인
+            if(name.contains(" ")) {
+                throw new IllegalArgumentException("자동차 이름에는 공백이 포함될 수 없습니다.");
+            }
+            String trimmedName = name.trim();
+            if (trimmedName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 빈 값일 수 없습니다.");
+            }
+
+            if (trimmedName.length() > 5) {
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+
+            if (nameList.contains(name)) {
+                throw new IllegalArgumentException("중복된 이름이 있습니다.");
+            }
+
+            nameList.add(name);
+        }
     }
 }
